3a45b5699b4f5b9a6c02135374147f79c323a43d
abuiliazeed <abuiliazeed@gmail.com>
2022-04-19 02:40:05 +0200

    update

--- src/models/user.model.ts
+++ src/models/user.model.ts
@@ -6,8 +6,10 @@ import dotenv from 'dotenv'
 
 dotenv.config()
 
+// creating a hash password to store in the database later
 const hashPassword = (password: string) => {
   const salt = Number(process.env.SALT_ROUNDS)
+  // we get the bcrypt password from the env file and we hash the password with the salt rounds
   return bcrypt.hashSync(`${password}${process.env.BCRYPT_PASSWORD}`, salt)
 }
 
@@ -17,9 +19,10 @@ class UserModel {
     try {
       //open connection with database
       const connection = await dbclient.connect()
-      //run query to insert new user
+      //run query to insert new user and return the user created
       const createUserQuery = `INSERT INTO users(firstname,lastname, email, password) VALUES($1, $2, $3, $4)
             RETURNING id,firstname,lastname,email`
+    // we feed the function the user we want to create
       const result = await connection.query(createUserQuery, [
         newUser.firstname,
         newUser.lastname,
@@ -69,25 +72,26 @@ class UserModel {
   }
 
   // update a user
-  async updateUser(u: User): Promise<User> {
+  async updateUser(user: User): Promise<User> {
     try {
       const connection = await dbclient.connect()
+      // we update the data feeded to the user with id stated and we return the user after editing
       const query = `UPDATE users 
                   SET email=$1, firstname=$2, lastname=$3, password=$4 
                   WHERE id=$5 
                   RETURNING id, email, firstname, lastname`
-
+// we feed the data to the function
       const result = await connection.query(query, [
-        u.email,
-        u.firstname,
-        u.lastname,
-        hashPassword(u.password as string),
-        u.id
+        user.email,
+        user.firstname,
+        user.lastname,
+        hashPassword(user.password as string),
+        user.id
       ])
       connection.release()
       return result.rows[0]
     } catch (error) {
-      throw new Error(`Could not update user: ${u.email}, ${(error as Error).message}`)
+      throw new Error(`Could not update user: ${user.email}, ${(error as Error).message}`)
     }
   }
 
@@ -113,14 +117,18 @@ class UserModel {
   async authenticateUser(email: string, password: string): Promise<User | null> {
     try {
       const connection = await dbclient.connect()
+      // this query will return the user with email stated
       const sql = 'SELECT password FROM users WHERE email=$1'
       const result = await connection.query(sql, [email])
+      // we will check if the user exist or not
       if (result.rows.length) {
         const { password: hashPassword } = result.rows[0]
+        // we will compare the provided credintials with the ones stored in the database by utilizing the bycrpt compare method
         const isPasswordValid = bcrypt.compareSync(
           `${password}${process.env.BCRYPT_PASSWORD}`,
           hashPassword
         )
+        // if the password is valid we will return the user info
         if (isPasswordValid) {
           const userInfo = await connection.query(
             'SELECT id, firstname, lastname, email FROM users WHERE email=($1)',
