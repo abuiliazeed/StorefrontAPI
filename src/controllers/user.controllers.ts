48e5df46918d3a546e2d5d29fbaaaf845216999e
abuiliazeed <abuiliazeed@gmail.com>
2022-04-19 02:45:47 +0200

    update

--- src/controllers/user.controllers.ts
+++ src/controllers/user.controllers.ts
@@ -9,7 +9,9 @@ const userModel = new UserModel()
 // create user function
 const createUser = async (req: Request, res: Response, next: NextFunction) => {
   try {
+    // fetching the data sent in the request body and passing it to the create user model function
     const user = await userModel.createUser(req.body)
+    // returning the user created
     res.json({
       status: 'success',
       data: { ...user },
@@ -23,6 +25,7 @@ const createUser = async (req: Request, res: Response, next: NextFunction) => {
 const getAllUsers = async (req: Request, res: Response, next: NextFunction) => {
   try {
     const users = await userModel.getAllUsers()
+    // returning all users
     res.json({
       status: 'success',
       data: { users },
@@ -35,7 +38,9 @@ const getAllUsers = async (req: Request, res: Response, next: NextFunction) => {
 // get user by id function
 const getUserById = async (req: Request, res: Response, next: NextFunction) => {
   try {
+    // fetching the user id from the request params
     const user = await userModel.getUserById(Number(req.params.id))
+    // returning the user
     res.json({
       status: 'success',
       data: { ...user },
@@ -48,7 +53,9 @@ const getUserById = async (req: Request, res: Response, next: NextFunction) => {
 // update a user
 const updateUser = async (req: Request, res: Response, next: NextFunction) => {
   try {
+    // fetching the user data that we want to update from the request body
     const user = await userModel.updateUser(req.body)
+    // returning the user updated
     res.json({
       status: 'success',
       data: user,
@@ -62,7 +69,9 @@ const updateUser = async (req: Request, res: Response, next: NextFunction) => {
 //delete a user
 const deleteUser = async (req: Request, res: Response, next: NextFunction) => {
   try {
+    // fetching the user id from the request params
     const user = await userModel.deleteUser(Number(req.params.id))
+    // returning the user deleted
     res.json({
       status: 'success',
       data: user,
@@ -76,13 +85,17 @@ const deleteUser = async (req: Request, res: Response, next: NextFunction) => {
 // authenticate user
 const authenticateUser = async (req: Request, res: Response, next: NextFunction) => {
   try {
+    // fetching the user email and password from the request body
     const user = await userModel.authenticateUser(req.body.email, req.body.password)
+    //generating a token based on the user data retrieved
     const token = jwt.sign({ user }, process.env.TOKEN_SECRET as string)
+    // if the user gave the wrong credentials we return an error
     if (!user) {
       res.status(401).json({
         status: 'error',
         message: 'invalid credentials'
       })
+      // if the user gave us the right credintials we return the user in addition to the token
     } else {
       res.json({
         status: 'success',
